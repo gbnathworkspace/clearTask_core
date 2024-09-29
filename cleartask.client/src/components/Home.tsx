1289d3ab59bf61f61cb7b57064f76c56f16a9775
LAPTOP-AAKD3SCI\91994 <lakshmi.p10@gmail.com>
2024-09-29 12:07:54 +0530

    test commit

--- cleartask.client/src/components/Home.tsx
+++ cleartask.client/src/components/Home.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        alert('Button Clicked');
+    }
+
+    const handleRegisterClick = () => {
+        navigate('/register');
+    }
+
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>This is a simple homepage for your application.</p>
+
+            <button onClick={handleClick}>Click Me</button>
+
+
+            <button onClick={handleRegisterClick}></button>
         </div>
     );
 };
